64a63fe470deaa8e03a5ee1fa0d93f69a2bb6bf7
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2026-07-28 14:02:41 -0600

    [feat][backend] agregar adapters de allauth para bloquear auto-registro
    - AccountAdapter/SocialAccountAdapter con is_open_for_signup=False
    - defensa en profundidad; el punto de aplicación real para el login de Google vía API es GoogleLoginSerializer (siguiente commit)
    
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

---
+++ backend/accounts/adapters.py
@@ -0,0 +1,12 @@
+from allauth.account.adapter import DefaultAccountAdapter
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+
+
+class AccountAdapter(DefaultAccountAdapter):
+    def is_open_for_signup(self, request):
+        return False
+
+
+class SocialAccountAdapter(DefaultSocialAccountAdapter):
+    def is_open_for_signup(self, request, sociallogin):
+        return False
