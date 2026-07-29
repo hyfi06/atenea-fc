1c9fa93888907dd7c75d0eccc0da1651c011d619
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2026-07-29 07:59:56 -0600

    [feat][frontend] configurar router en App.tsx
    - BrowserRouter con / (Landing), /login (Login), /health (HealthCheck, sin cambios)
    
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Landing } from './screens/Landing'
+import { Login } from './screens/Login'
 import { HealthCheck } from './screens/HealthCheck'
 
 function App() {
-  return <HealthCheck />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/health" element={<HealthCheck />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
